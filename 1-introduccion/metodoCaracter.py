5d801b6d0e9925e9107370327c38b8e553010ca2
Alexpc <manebc121@gmail.com>
2024-05-27 10:09:13 -0700

    actualizacion

---
+++ 1-introduccion/metodoCaracter.py
@@ -0,0 +1,7 @@
+cadena = "estoY prObando metodos de python"
+
+print(cadena.lower())  #Todo en minusulas
+print(cadena.upper())  #Todo en mayusculas
+print(cadena.capitalize()) #Primer letra mayuscula 
+print(cadena.title()) #Primer letra de cada palabra mayuscula 
+print(cadena.swapcase()) #las letras minusculas a mayusculas y viuseverza
---
+++ Ejercicios/02-ejercisios.py
@@ -0,0 +1,22 @@
+"""Crear un programa, que tenga una variable con la cadena “Te quiero solo como amigo” """
+
+#• Imprima los dos primeros caracteres.
+cadena = "Te quiero solo como amigo"
+print(cadena[0:3])
+
+#• Imprima los tres últimos caracteres.
+cadena = "Te quiero solo como amigo"
+print(cadena[-3: ])
+
+#• Imprima dicha cadena cada dos caracteres. Ej.: Si la cadena fuera “recta” debería imprimir rca
+cadena = "Te quiero solo como amigo"
+print(cadena[: : 2])
+
+#• Dicha cadena en sentido inverso.
+cadena = "Te quiero solo como amigo"
+print(cadena[: : -1])
+
+cadena = "Te quiero solo como amigo"
+print(cadena[: : -1])
+print(cadena[: : 1])
+
