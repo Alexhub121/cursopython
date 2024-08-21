bcb9f06fb40c2f4e285f1480098102a7ddb25c1b
Alexhub121 <manebc121@gmail.com>
2024-08-20 20:52:46 -0600

    FuncionesCreadas

--- Funciones/FuncionesCreadas.py
+++ Funciones/FuncionesCreadas.py
@@ -1,4 +1,11 @@
-def saludo():
+def saludo(): #Nombre de la funcion
     print("Hola Alex")
 
-saludo()
+saludo() #LLamar a la funcion
+
+def tabla7(): #Nombre de la funcion
+    for i in range(11):
+        multi = i * 7
+        print(f"7 x {i} = {multi}")
+
+tabla7() #Llamar a la funcion
